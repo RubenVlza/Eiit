654b4a18b2d64a8b5e03bf1d0c152206874ebeee
Ruben V <Ruben V>
2017-04-22 15:50:02 -0600

    menu update

--- js/controllers/SetupCtrl.js
+++ js/controllers/SetupCtrl.js
@@ -4,3 +4,22 @@
     });
     */
 });
+
+var dataRef = new Firebase('https://eiit-56995.firebaseio.com');
+
+facebookConnectPlugin.login(['public_info'], function(status) {
+  facebookConnectPlugin.getAccessToken(function(token) {
+    // Authenticate with Facebook using an existing OAuth 2.0 access token
+    dataRef.authWithOAuthToken("facebook", token, function(error, authData) {
+      if (error) {
+        console.log('Firebase login failed!', error);
+      } else {
+        console.log('Authenticated successfully with payload:', authData);
+      }
+    });
+  }, function(error) {
+    console.log('Could not get access token', error);
+  });
+}, function(error) {
+  console.log('An error occurred logging the user in', error);
+});
